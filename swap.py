c86366fb72c8d6d066ce116867ffbdd226a168a1
AasthaAgarwal <aastha.agarwal90@gmail.com>
2017-09-15 15:14:39 +0530

    swap updated

--- swap.py
+++ swap.py
@@ -1,7 +1,8 @@
-a=int(raw_input("enter a number"))
-b=int(raw_input("enter the othe no."))
+a=int(raw_input("enter a number "))
+b=int(raw_input("enter the othe no. "))
 
 a=a+b
 b=a-b
 a=a-b
-print +a +b
+print 'Swapped value of a : ', a
+print 'Swapped value of b : ', b
